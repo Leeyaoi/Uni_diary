4bef2bdcce7b99416d6e8e630bb1c14fd8609213
Leeyaoi <darya.ya2608@gmail.com>
2024-10-22 22:06:30 +0300

    added admin post check

--- Uni_diary.API/controllers/adminController.ts
+++ Uni_diary.API/controllers/adminController.ts
@@ -1,14 +1,53 @@
+import * as express from "express";
+import { v4 as uuidv4 } from "uuid";
 import { Admin } from "../dbModels/admin";
 import CreateAdminDto from "../DTOs/AdminDtos/CreateAdminDto";
 import UpdateAdminDto from "../DTOs/AdminDtos/UpdateAdminDto";
 import { CreateAdminValidator } from "../validators/AdminValidators/CreateAdminValidator";
 import { UpdateAdminValidator } from "../validators/AdminValidators/UpdateAdminValidator";
-import GenericController from "./genericController";
+import GenericController, { myValidationResult } from "./genericController";
+import { checkSchema } from "express-validator";
+import createHttpError = require("http-errors");
+import GenericRepository from "../repositories/GenericRepository";
 
 const adminController = new GenericController<CreateAdminDto, UpdateAdminDto>(
   Admin,
   CreateAdminValidator,
   UpdateAdminValidator
 ).GenerateController();
+
+adminController.post(
+  "/",
+  express.urlencoded({ extended: false }),
+  checkSchema(CreateAdminValidator),
+  async (
+    req: express.Request<{}, {}, CreateAdminDto>,
+    res: express.Response,
+    next
+  ) => {
+    const errors: string[] = myValidationResult(req).array();
+    if (errors.length != 0) {
+      next(createHttpError(400, JSON.stringify(errors)));
+      return;
+    }
+
+    const repo = new GenericRepository(Admin);
+
+    const newModel = {
+      ...req.body,
+      id: uuidv4(),
+    };
+
+    const search = await repo.getByPredicate({ userId: newModel.userId }, []);
+
+    if (search != "[]") {
+      next(createHttpError(400, "admin with this userId already exists"));
+      return;
+    }
+
+    const data = await repo.create(newModel);
+    res.send(data);
+  }
+);
 
 export default adminController;
--- Uni_diary.API/controllers/userController.ts
+++ Uni_diary.API/controllers/userController.ts
@@ -1,14 +1,54 @@
+import * as express from "express";
+import createHttpError = require("http-errors");
+import { v4 as uuidv4 } from "uuid";
 import { User } from "../dbModels/user";
 import CreateUserDto from "../DTOs/UserDtos/CreateUserDto";
 import UpdateUserDto from "../DTOs/UserDtos/UpdateUserDto";
 import { CreateUserValidator } from "../validators/UserValidators/CreateUserValidator";
 import { UpdateUserValidator } from "../validators/UserValidators/UpdateUserValidator";
 import GenericController from "./genericController";
+import { checkSchema, validationResult } from "express-validator";
+import GenericRepository from "../repositories/GenericRepository";
+import { Teacher } from "../dbModels/teacher";
+import { Admin } from "../dbModels/admin";
+import { Student } from "../dbModels/student";
+
+const urlencodedParser = express.urlencoded({ extended: false });
+const repo = new GenericRepository(User);
 
 const userController = new GenericController<CreateUserDto, UpdateUserDto>(
   User,
   CreateUserValidator,
   UpdateUserValidator
 ).GenerateController();
+
+//AUTH
+userController.post(
+  "/auth",
+  urlencodedParser,
+  checkSchema(CreateUserValidator),
+  async (
+    req: express.Request<{}, {}, CreateUserDto>,
+    res: express.Response,
+    next
+  ) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      next(createHttpError(400, "Validation error"));
+      return;
+    }
+
+    const newModel = {
+      ...req.body,
+    };
+
+    const data = await repo.getByPredicate(newModel, [Student, Teacher, Admin]);
+    if (data == "[]") {
+      next(createHttpError(404, "NotFound"));
+      return;
+    }
+    res.send(data);
+  }
+);
 
 export default userController;
