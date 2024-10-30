a92c8718c9db2357fd5863639cf6deecb048706d
Leeyaoi <darya.ya2608@gmail.com>
2024-10-30 19:37:02 +0300

    added sessionStorage save

--- Uni_diary.Front/src/shared/stores/store.ts
+++ Uni_diary.Front/src/shared/stores/store.ts
@@ -1,12 +1,32 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { combineReducers, configureStore } from "@reduxjs/toolkit";
+import { persistStore, persistReducer } from "redux-persist";
+import storageSession from "redux-persist/lib/storage/session";
 import { userSlice } from "./userSlice";
 import { TypedUseSelectorHook, useDispatch, useSelector } from "react-redux";
 
-export const store = configureStore({
-  reducer: {
+const persistConfig = {
+  key: "root",
+  storage: storageSession,
+};
+
+const persistedReducer = persistReducer(
+  persistConfig,
+  combineReducers({
     user: userSlice.reducer,
-  },
+  })
+);
+export const store = configureStore({
+  reducer: persistedReducer,
+  devTools: process.env.NODE_ENV !== "production",
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: ["persist/PERSIST", "persist/REHYDRATE"],
+      },
+    }),
 });
+
+export const persistor = persistStore(store);
 
 export const useAppDispatch: () => typeof store.dispatch = useDispatch;
 export const useAppSelector: TypedUseSelectorHook<
